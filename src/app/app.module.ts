3ab30a2036e9efa8b9c5684d281afc45caf38b03
TheusMarqs <matheusmarqsilva@gmail.com>
2023-03-31 15:00:17 -0300

    angular1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { FatecComponent } from './fatec/fatec.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FatecComponent
   ],
   imports: [
     BrowserModule
